ee44cd08dd824770af80191c32585d62c0209e49
vivijie <j.uloveit@gmail.com>
2017-09-16 16:01:15 +0800

    - Prepare add coreDataStack

--- DaysLeft/AppDelegate.swift
+++ DaysLeft/AppDelegate.swift
@@ -45,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.saveContext()
     }
 
+
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
@@ -59,7 +60,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -91,4 +92,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
+
 
---
+++ DaysLeft/CoreDataStack.swift
@@ -0,0 +1,45 @@
+//
+//  CoreDataStack.swift
+//  DaysLeft
+//
+//  Created by viviJIE on 9/16/17.
+//  Copyright © 2017 tdones. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataStack {
+  private let modelName: String
+  init(modelName: String) {
+    self.modelName = modelName
+  }
+  
+  private lazy var storeContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: self.modelName)
+    container.loadPersistentStores{
+      (storeDescription, error) in
+      if let error = error as NSError? {
+        print("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  lazy var managedContext: NSManagedObjectContext = {
+    return self.storeContainer.viewContext
+  }()
+  
+  func saveContext() {
+    guard managedContext.hasChanges else {
+      return
+    }
+    
+    do {
+      try managedContext.save()
+    } catch let error as NSError {
+      print("Unresolved error \(error), \(error.userInfo)")
+    }
+  }
+}
